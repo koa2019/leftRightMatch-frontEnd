608b6a294245c5420e08720e36b388557d88c698
koa2019 <no-reply>
2019-11-12 19:04:57 -0800

    rendered generic ProfileCard compontent to userProfile.js

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,8 @@ function App() {
             <Route exact path="/candidatesprofile" component={CandidateProfile} />
             <Route exact path="/login" component={LoginSignUp} />
             <Route path="/logout" component={Logout} />
-            <Route exact path="/userprofile/:id" component={UserProfile} />
+            <Route exact path="/userprofile/" component={UserProfile} />
+            {/* <Route exact path="/userprofile/:id" component={UserProfile} /> */}
             <Route component={NoMatch} />
           </Switch>
         </div>
